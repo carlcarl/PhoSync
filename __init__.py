b07dd64d9322306e817b4690760e11b188ee71ef
carlcarl <carlcarlking@gmail.com>
2013-07-16 23:54:43 +0800

    First commit

---
+++ __init__.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import locale
+from ConfigParser import SafeConfigParser
+from dropbox import client
+
+
+class CaCaSync(object):
+    CONF_FILE = 'cacasync.conf'
+
+    def __init__(self):
+        parser = SafeConfigParser()
+        parser.read(self.CONF_FILE)
+
+        self.app_key = parser.get('dropbox', 'APP_KEY')
+        self.app_secret = parser.get('dropbox', 'APP_SECRET')
+        self.current_path = ''
+
+        self.api_client = None
+        try:
+            token = parser.get('dropbox', 'APP_TOKEN')
+            self.api_client = client.DropboxClient(token)
+            print "[loaded access token]"
+        except IOError:
+            pass  # don't worry if it's not there
+
+    def ls(self):
+        resp = self.api_client.metadata('')
+
+        if 'contents' in resp:
+            for f in resp['contents']:
+                name = os.path.basename(f['path'])
+                encoding = locale.getdefaultlocale()[1]
+                print(('%s' % name).encode(encoding))
+
+    def get(self, from_path, to_path):
+        """
+        Copy file from Dropbox to local file and print out the metadata.
+
+        Examples:
+        Dropbox> get file.txt ~/dropbox-file.txt
+        """
+        to_file = open(os.path.expanduser(to_path), "wb")
+
+        f, metadata = self.api_client.get_file_and_metadata(
+            self.current_path + "/" + from_path
+        )
+        print 'Metadata:', metadata
+        to_file.write(f.read())
+
+
+def main():
+    cacasync = CaCaSync()
+    cacasync.ls()
+
+if __name__ == '__main__':
+    main()
