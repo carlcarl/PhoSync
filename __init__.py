4d642f1d897e8ae6d513e4217c6c7d505fed7e2d
carlcarl <carlcarlking@gmail.com>
2013-07-19 02:43:32 +0800

    Separate into Dropbox and CaCaSync class

--- __init__.py
+++ __init__.py
@@ -2,37 +2,52 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import locale
+import tempfile
 from ConfigParser import SafeConfigParser
 from dropbox import client
 
+CONF_FILE = 'cacasync.conf'
+
 
 class CaCaSync(object):
-    CONF_FILE = 'cacasync.conf'
+    def __init__():
+        pass
 
-    def __init__(self):
-        parser = SafeConfigParser()
-        parser.read(self.CONF_FILE)
 
-        self.dropbox_key = parser.get('dropbox', 'APP_KEY')
-        self.dropbox_secret = parser.get('dropbox', 'APP_SECRET')
-        self.dropbox_current_path = ''
-        self.dropbox_api_client = None
-        try:
-            token = parser.get('dropbox', 'APP_TOKEN')
-            self.dropbox_api_client = client.DropboxClient(token)
-            print "[loaded access token]"
-        except IOError:
-            pass  # don't worry if it's not there
+class Dropbox(object):
 
-    def ls(self):
-        resp = self.dropbox_api_client.metadata('')
+    def __init__(
+        self, dropbox_key, dropbox_secret, dropbox_token, dropbox_current_path
+    ):
+        self.dropbox_key = dropbox_key
+        self.dropbox_secret = dropbox_secret
+        self.dropbox_token = dropbox_token
+        self.dropbox_current_path = dropbox_current_path
 
+        self.dropbox_api_client = client.DropboxClient(dropbox_token)
+        tmp_dir = tempfile.gettempdir() + os.sep + 'cacasync'
+        if os.path.exists(tmp_dir):
+            if not os.path.isdir(tmp_dir):
+                sys.exit(1)
+        else:
+            os.mkdir(tmp_dir)
+        print(tmp_dir)
+        self.tmp_dir = tmp_dir
+        print "[loaded access token]"
+
+    def ls(self, path):
+        resp = self.dropbox_api_client.metadata(
+            self.dropbox_current_path + os.sep + path
+        )
+        file_list = []
         if 'contents' in resp:
             for f in resp['contents']:
                 name = os.path.basename(f['path'])
                 encoding = locale.getdefaultlocale()[1]
-                print(('%s' % name).encode(encoding))
+                file_list.append(('%s' % name).encode(encoding))
+        return file_list
 
     def get(self, from_path, to_path):
         """
@@ -41,18 +56,39 @@ class CaCaSync(object):
         Examples:
         Dropbox> get file.txt ~/dropbox-file.txt
         """
-        to_file = open(os.path.expanduser(to_path), "wb")
+        to_file = open(os.path.expanduser(to_path), 'wb')
 
         f, metadata = self.dropbox_api_client.get_file_and_metadata(
-            self.dropbox_current_path + "/" + from_path
+            self.dropbox_current_path + os.sep + from_path
         )
-        print 'Metadata:', metadata
+        # print 'Metadata:', metadata
         to_file.write(f.read())
+
+    def get_folder(self, from_path):
+        file_list = self.ls(from_path)
+        to_path = self.tmp_dir + os.sep + from_path
+        os.makedirs(to_path)
+        for f in file_list:
+            self.get(from_path + os.sep + f, to_path + os.sep + f)
 
 
 def main():
-    cacasync = CaCaSync()
-    cacasync.ls()
+    parser = SafeConfigParser()
+    parser.read(CONF_FILE)
+
+    dropbox_key = parser.get('dropbox', 'APP_KEY')
+    dropbox_secret = parser.get('dropbox', 'APP_SECRET')
+
+    dropbox_token = parser.get('dropbox', 'APP_TOKEN')
+    dropbox_current_path = parser.get('dropbox', 'CURRENT_PATH')
+
+    dropbox = Dropbox(
+        dropbox_key,
+        dropbox_secret,
+        dropbox_token,
+        dropbox_current_path
+    )
+    dropbox.get_folder('test')
 
 if __name__ == '__main__':
     main()
